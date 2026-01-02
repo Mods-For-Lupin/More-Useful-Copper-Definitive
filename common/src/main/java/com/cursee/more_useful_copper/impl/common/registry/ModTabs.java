73dd11c7eea12de89d17df4e80e14bbcea7d6a93
jason13official <jason13official@gmail.com>
2026-01-02 18:31:08 -0500

    feat: creative mode tab, translations

--- common/src/main/java/com/cursee/more_useful_copper/impl/common/registry/ModTabs.java
+++ common/src/main/java/com/cursee/more_useful_copper/impl/common/registry/ModTabs.java
@@ -1,11 +1,33 @@
 package com.cursee.more_useful_copper.impl.common.registry;
 
+import com.cursee.more_useful_copper.platform.Services;
 import java.util.function.BiConsumer;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 
 public class ModTabs {
 
-  public static void register(BiConsumer<CreativeModeTab, ResourceLocation> consumer) {}
+  public static CreativeModeTab MORE_USEFUL_COPPER;
+
+  public static void register(BiConsumer<CreativeModeTab, ResourceLocation> consumer) {
+
+    MORE_USEFUL_COPPER = Services.PLATFORM.createCreativeTabBuilder()
+        .icon(() -> new ItemStack(ModItems.MOISTURE_COMPASS))
+        .title(Component.translatable("itemGroup.moreUsefulCopper"))
+        .displayItems((itemDisplayParameters, output) -> {
+
+          output.accept(ModItems.COPPER_STATUE_CREEPER);
+          output.accept(ModItems.COPPER_STATUE_SKELETON);
+          output.accept(ModItems.COPPER_STATUE_SPIDER);
+          output.accept(ModItems.COPPER_STATUE_ZOMBIE);
+
+          output.accept(ModItems.MOISTURE_COMPASS);
+          output.accept(ModItems.COPPER_BELL);
+        })
+        .build();
+
+  }
 }
